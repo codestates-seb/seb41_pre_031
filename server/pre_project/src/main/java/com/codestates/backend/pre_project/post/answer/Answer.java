25cdd72b3510c163ed24be6e274fff38299c4887
kimjeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-12-20 22:54:44 +0900

    [BE feat] entity클래스 구현

---
+++ server/pre_project/src/main/java/com/codestates/backend/pre_project/post/answer/Answer.java
@@ -0,0 +1,55 @@
+package com.codestates.backend.pre_project.post.answer;
+
+import com.codestates.backend.pre_project.likes.entity.Likes;
+import com.codestates.backend.pre_project.member.entity.Member;
+import com.codestates.backend.pre_project.post.comment.Comment;
+import com.codestates.backend.pre_project.post.question.Question;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long answerId;
+
+    @Column(nullable = false, length = 500)
+    private String answerBody;
+
+    @Column(nullable = false)
+    private long answerLikes;
+
+    @CreatedDate
+    private LocalDateTime answerRegDate;
+
+    @LastModifiedDate
+    private LocalDateTime answerLastDate;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+
+    @OneToMany(mappedBy = "answer", cascade = CascadeType.ALL)
+    private List<Likes> likes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "answer", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
+//  @Column(nullable = false)
+//  private long memberId;
+}
---
+++ server/pre_project/src/main/java/com/codestates/backend/pre_project/post/question/QuestionTag.java
@@ -0,0 +1,27 @@
+package com.codestates.backend.pre_project.post.question;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionTag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long questionTagId;
+
+    @ManyToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+
+    @ManyToOne
+    @JoinColumn(name = "TAG_ID")
+    private Tag tag;
+}
