39fd81f5b55a8d38ceee03439fdcfb04e25a8858
kimjeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-12-21 23:18:01 +0900

    [BE feat] member controller test 구현

---
+++ server/pre_project/src/main/java/com/codestates/backend/pre_project/member/dto/MemberDto.java
@@ -0,0 +1,45 @@
+package com.codestates.backend.pre_project.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class MemberDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class Post {
+        @NotBlank(message = "이메일은 공백이 아니어야 합니다.")
+        @Email
+        private String email;
+
+        @NotBlank(message = "비밀번호는 공백이 아니어야 합니다.")
+        private String password;
+
+        @NotBlank(message = "이름은 공백이 아니어야 합니다.")
+        private String memberName;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Patch {
+        private long memberId;
+        
+        private String password;
+
+        private String memberName;
+
+        public void setMemberId(long memberId) {this.memberId = memberId;}
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response {
+        private long memberId;
+        private String email;
+        private String memberName;
+        private String pointCount;
+    }
+}
