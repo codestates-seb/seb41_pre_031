a47542f0f3ffcd3ab6a1c71c315cea919c41493b
rlawodn46465 <rlawodn46465@naver.com>
2022-12-21 14:10:01 +0900

    [FE feat] 레이아웃 구축

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Section = styled.div`
+  box-sizing: border-box;
+  border-left: ${(props) => props.flag ? '1px solid rgb(214 217 220)' : '0px'};
+  max-width: ${(props) => props.flag ? "1100px" : "1854px"};
+  width: ${(props) => props.flag ? "calc(100% - 164px)" : "100%"};
+  height: 100%;
+
+  @media screen and (max-width:640px){
+    width: 100%
+  }
+`;
 
 function App() {
+  const [flag, setFlag] = useState(true);
+  //flag : nav 유무 조작
   return (
-    <div></div>
+    <>
+      <input></input>
+      {/* 헤더 */}
+      <main>
+        {flag ? <nav></nav> : null}
+        <Section flag={flag}></Section>
+      </main>
+      {/* 푸터 */}
+    </>
   );
 }
 
