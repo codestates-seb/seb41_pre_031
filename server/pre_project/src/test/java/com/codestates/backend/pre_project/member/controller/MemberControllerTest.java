8828462a6a63a151e08831bb7dc94db7c58739f6
superscman <vdcf2000@gmail.com>
2022-12-22 09:02:36 +0900

    [BE feat] member class testing getmembers delete 구현

--- server/pre_project/src/test/java/com/codestates/backend/pre_project/member/controller/MemberControllerTest.java
+++ server/pre_project/src/test/java/com/codestates/backend/pre_project/member/controller/MemberControllerTest.java
@@ -3,14 +3,18 @@ package com.codestates.backend.pre_project.member.controller;
 import com.codestates.backend.pre_project.member.dto.MemberDto;
 import com.codestates.backend.pre_project.member.entity.Member;
 import com.codestates.backend.pre_project.member.mapper.MemberMapper;
+import com.codestates.backend.pre_project.member.repository.MemberRepository;
+import com.codestates.backend.pre_project.member.service.MemberService;
 import com.google.gson.Gson;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -36,6 +40,12 @@ public class MemberControllerTest {
         private Gson gson;
 
         @Autowired
+        private MemberService memberService;
+
+        @Autowired
+        private MemberRepository memberRepository;
+
+        @Autowired
         private MemberMapper mapper;
 
         @Test
@@ -59,7 +69,9 @@ public class MemberControllerTest {
                 //then
                 actions
                         .andExpect(status().isCreated())
-                        .andExpect(jsonPath("$.data.email").value(post.getEmail()));
+                        .andExpect(jsonPath("$.data.email").value(post.getEmail()))
+                        .andExpect(jsonPath("$.data.password").value(post.getPassword()))
+                        .andExpect(jsonPath("$.data.memberName").value(post.getMemberName()));
         }
 
         @Test
@@ -93,7 +105,7 @@ public class MemberControllerTest {
                     )
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.data.email").value(resultMember.getEmail()))
-                    .andExpect(jsonPath("$.data.name").value(resultMember.getName()));
+                    .andExpect(jsonPath("$.data.memberName").value(resultMember.getMemberName()));
 
 
         }
@@ -101,18 +113,62 @@ public class MemberControllerTest {
     @Test
         void getMembersTest() throws Exception {
                 //given
+            MemberDto.Post post1 = new MemberDto.Post("abcd1@gmail.com", "qhdks!1234", "강신찬");
+            MemberDto.Post post2 = new MemberDto.Post("abcd2@gmail.com", "qhdks!1234", "김정희");
+            MemberDto.Post post3 = new MemberDto.Post("abcd3@gmail.com", "qhdks!1234", "김현성");
+
+            Member member1 = mapper.memberPostDtoToMember(post1);
+            Member member2 = mapper.memberPostDtoToMember(post2);
+            Member member3 = mapper.memberPostDtoToMember(post3);
+
+            Member resultMember1 = memberRepository.save(member1);
+            Member resultMember2 = memberRepository.save(member2);
+            Member resultMember3 = memberRepository.save(member3);
+
 
                //when
+        ResultActions actions =
+                mockMvc.perform(
+                        get("/members")
+                                .param("page","1")
+                                .param("size","10")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                );
 
                 //then
+        MvcResult result = actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+        System.out.println(content);
+        System.out.println("*******************************");
         }
 
         @Test
         void deleteMemberTest() throws Exception {
                 //given
+            MemberDto.Post post = new MemberDto.Post("vdcf2000@gmail.com",
+                    "asdf123","강신찬");
+            Member member = mapper.memberPostDtoToMember(post);
+            Member resultMember1 = memberRepository.save(member);
+
+            long memberId = memberRepository.findByEmail("vdcf2000@gmail.com").get().getMemberId();
 
                 //when
+            ResultActions actions =
+                    mockMvc.perform(
+                    delete("/members/" + memberId)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+            );
 
                 //then
+            MvcResult result = actions
+                    .andExpect(status().isNoContent())
+                    .andExpect(jsonPath("$").doesNotExist())
+                    .andReturn();
+
         }
 }
