a7620ac02f5707fcff3e4dc9f3e5c6af76bdb198
kimjeonghui <20190896@sungshin.ac.kr>
2022-12-22 20:55:32 +0900

    [BE feat] Member api 자동화 구현

--- server/pre_project/src/test/java/com/codestates/backend/pre_project/member/controller/MemberControllerTest.java
+++ server/pre_project/src/test/java/com/codestates/backend/pre_project/member/controller/MemberControllerTest.java
@@ -15,12 +15,18 @@ import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.ParameterDescriptor;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -33,54 +39,69 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.transaction.Transactional;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static com.codestates.backend.pre_project.util.ApiDocumentUtils.getRequestPreProcessor;
+import static com.codestates.backend.pre_project.util.ApiDocumentUtils.getResponsePreProcessor;
+import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.JsonFieldType.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MemberControllerTest {
-    
-        @Autowired
-        private MockMvc mockMvc;
+@AutoConfigureRestDocs
+public class MemberControllerTest implements MemberControllerTestHelper {
 
-        @Autowired
-        private Gson gson;
+    @Autowired
+    private MockMvc mockMvc;
 
-        @MockBean
-        private MemberService memberService;
+    @Autowired
+    private Gson gson;
 
-        @MockBean
-        private MemberMapper mapper;
+    @MockBean
+    private MemberService memberService;
 
-        @Test
-        void postMemberTest() throws Exception {
-                //given
-                MemberDto.Post post = (MemberDto.Post) StubData.MockMember.getRequestBody(HttpMethod.POST);
-                MemberDto.Response responseBody = StubData.MockMember.getSingleResponseBody();
+    @MockBean
+    private MemberMapper mapper;
 
-                given(mapper.memberPostDtoToMember(Mockito.any(MemberDto.Post.class))).willReturn(new Member());
+    @Test
+    void postMemberTest() throws Exception {
+        //given
+        MemberDto.Post post = (MemberDto.Post) StubData.MockMember.getRequestBody(HttpMethod.POST);
+        MemberDto.Response responseBody = StubData.MockMember.getSingleResponseBody();
 
-                Member mockResultMember = new Member();
-                mockResultMember.setMemberId(1L);
-                given(memberService.createMember(Mockito.any(Member.class))).willReturn(mockResultMember);
-                given(mapper.memberToMemberResponseDto(Mockito.any(Member.class))).willReturn(responseBody);
+        given(mapper.memberPostDtoToMember(Mockito.any(MemberDto.Post.class))).willReturn(new Member());
 
-                String content = gson.toJson(post);
-                //URI uri = getURI();
+        Member mockResultMember = new Member();
+        mockResultMember.setMemberId(1L);
+        given(memberService.createMember(Mockito.any(Member.class))).willReturn(mockResultMember);
+        given(mapper.memberToMemberResponseDto(Mockito.any(Member.class))).willReturn(responseBody);
 
-                //when
-                ResultActions actions =
-                        mockMvc.perform (
-                                post("/members/signup")
-                                        .accept(MediaType.APPLICATION_JSON)
-                                        .contentType(MediaType.APPLICATION_JSON)
-                                        .content(content)
-                        );
+        String content = gson.toJson(post);
+        //URI uri = getURI();
+
+        //when
+        ResultActions actions =
+                mockMvc.perform(
+                        post("/members/signup")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                );
 
                 //then
                 actions
@@ -90,60 +111,83 @@ public class MemberControllerTest {
                         .andExpect(jsonPath("$.data.memberName").value(post.getMemberName()));
         }
 
-        @Test
-        void patchMemberTest() throws Exception {
-                //given
-            long memberId = 1L;
+    @Test
+    void patchMemberTest() throws Exception {
+        //given
+        long memberId = 1L;
 
-            MemberDto.Patch patch = (MemberDto.Patch) StubData.MockMember.getRequestBody(HttpMethod.PATCH);
-            MemberDto.Response response =
-                    StubData.MockMember.getSingleResponseBody(memberId, null, "김현성", "asdf123");
+        MemberDto.Patch patch = (MemberDto.Patch) StubData.MockMember.getRequestBody(HttpMethod.PATCH);
+        MemberDto.Response response =
+                StubData.MockMember.getSingleResponseBody(memberId, null, "김현성", "asdf123");
 
-            given(mapper.memberPatchDtoToMember(Mockito.any(MemberDto.Patch.class))).willReturn(new Member());
+        given(mapper.memberPatchDtoToMember(Mockito.any(MemberDto.Patch.class))).willReturn(new Member());
 
-            given(memberService.updateMember(Mockito.any(Member.class))).willReturn(new Member());
+        given(memberService.updateMember(Mockito.any(Member.class))).willReturn(new Member());
 
-            given(mapper.memberToMemberResponseDto(Mockito.any(Member.class))).willReturn(response);
+        given(mapper.memberToMemberResponseDto(Mockito.any(Member.class))).willReturn(response);
 
-            String content = gson.toJson(patch);
+        String content = gson.toJson(patch);
 
-                //when
-            ResultActions actions = mockMvc.perform(
-                    patch("/members/{member_id}",memberId)
-                            .accept(MediaType.APPLICATION_JSON)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(content)
-            );
+        //when
+        ResultActions actions = mockMvc.perform(patchRequestBuilder(getURI(), memberId, content));
 
-                //then
-            actions.andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.memberName").value(patch.getMemberName()))
-                    .andExpect(jsonPath("$.data.password").value(patch.getPassword()));
-        }
+        //then
+        actions.andExpect(status().isOk())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.memberId").value(patch.getMemberId()))
+                .andExpect(jsonPath("$.data.memberName").value(patch.getMemberName()))
+                .andExpect(jsonPath("$.data.password").value(patch.getPassword()))
+                .andDo(document("patch-member",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                getMemberRequestPathParameterDescriptor()
+                        ),
+                        requestFields(
+                                getDefaultMemberPatchRequestDescriptors()
+                        ),
+                        responseFields(
+                                getFullResponseDescriptors(
+                                        getDefaultMemberResponseDescriptors(DataResponseType.SINGLE))
+                        )
+                ));
+    }
 
+    @Test
+    void getMemberTest() throws Exception {
+        //given
+        long memberId = 1L;
+        Member member = StubData.MockMember.getSingleResultMember(memberId);
+        MemberDto.Response response = StubData.MockMember.getSingleResponseBody();
 
-        @Test
-        void getMemberTest() throws Exception {
-            //given
-            long memberId = 1L;
-            Member member = StubData.MockMember.getSingleResultMember(memberId);
-            MemberDto.Response response = StubData.MockMember.getSingleResponseBody();
+        given(memberService.findMember(Mockito.anyLong())).willReturn(new Member());
+        given(mapper.memberToMemberResponseDto(Mockito.any(Member.class))).willReturn(response);
 
-            given(memberService.findMember(Mockito.anyLong())).willReturn(new Member());
-            given(mapper.memberToMemberResponseDto(Mockito.any(Member.class))).willReturn(response);
+        //when
+        ResultActions actions = mockMvc.perform(
+                getRequestBuilder(getURI(), memberId));
 
-            //when
-            ResultActions actions = mockMvc.perform(
-                    get("/members/{member_id}",memberId)
-                            .accept(MediaType.APPLICATION_JSON));
+        //then
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.email").value(member.getEmail()))
+                .andExpect(jsonPath("$.data.memberName").value(member.getMemberName()))
+                .andExpect(jsonPath("$.data.password").value(member.getPassword()))
+                .andDo(
+                        document("get-member",
+                                getRequestPreProcessor(),
+                                getResponsePreProcessor(),
+                                pathParameters(
+                                        getMemberRequestPathParameterDescriptor()
+                                ),
+                        responseFields(
+                                getFullResponseDescriptors(
+                                        getDefaultMemberResponseDescriptors(DataResponseType.SINGLE)
+                                )
+                        )
 
-            //then
-            actions.andExpect(status().isOk())
-                    .andExpect(jsonPath("$.data.email").value(member.getEmail()))
-                    .andExpect(jsonPath("$.data.memberName").value(member.getMemberName()))
-                    .andExpect(jsonPath("$.data.password").value(member.getPassword()));
+                ));
 
-            // given
+//            // given
 //            MemberDto.Post post = new MemberDto.Post("vdcf2000@gmail.com",
 //                    "asdf123","강신찬");
 //            Member member = mapper.memberPostDtoToMember(post);
@@ -161,13 +205,13 @@ public class MemberControllerTest {
 //                    .andExpect(status().isOk())
 //                    .andExpect(jsonPath("$.data.email").value(resultMember.getEmail()))
 //                    .andExpect(jsonPath("$.data.memberName").value(resultMember.getMemberName()));
+//
 
-
-        }
+    }
 
     @Test
-        void getMembersTest() throws Exception {
-            //give
+    void getMembersTest() throws Exception {
+        //give
         Page<Member> pageMembers = StubData.MockMember.getMultiResultMember();
 
         List<MemberDto.Response> responses = StubData.MockMember.getMultiResponseBody();
@@ -182,13 +226,10 @@ public class MemberControllerTest {
         queryParams.add("size", size);
 
 
-            //when
-        ResultActions actions = mockMvc.perform(
-                get("/members")
-                        .params(queryParams)
-                        .accept(MediaType.APPLICATION_JSON));
+        //when
+        ResultActions actions = mockMvc.perform(getRequestBuilder(getUrl(),queryParams));
 
-            //then
+        //then
         MvcResult result = actions
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data").isArray())
@@ -198,7 +239,7 @@ public class MemberControllerTest {
         System.out.println(list);
         MatcherAssert.assertThat(list.size(), is(2));
 
-                //given
+        //given
 //            MemberDto.Post post1 = new MemberDto.Post("abcd1@gmail.com", "qhdks!1234", "강신찬");
 //            MemberDto.Post post2 = new MemberDto.Post("abcd2@gmail.com", "qhdks!1234", "김정희");
 //            MemberDto.Post post3 = new MemberDto.Post("abcd3@gmail.com", "qhdks!1234", "김현성");
@@ -230,19 +271,29 @@ public class MemberControllerTest {
 //        String content = result.getResponse().getContentAsString();
 //        System.out.println(content);
 //        System.out.println("*******************************");
-        }
+    }
 
-        @Test
-        void deleteMemberTest() throws Exception {
-            //given
-            long memberId = 1L;
-            doNothing().when(memberService).deleteMember(memberId);
-            //when
-            ResultActions actions = mockMvc.perform(delete("/members/{member-id}", memberId));
-            //then
-            actions.andExpect(status().isNoContent());
+    @Test
+    void deleteMemberTest() throws Exception {
+        //given
+        long memberId = 1L;
+        doNothing().when(memberService).deleteMember(memberId);
+        //when
+        ResultActions actions = mockMvc.perform(deleteRequestBuilder(getURI(), memberId));
+        //then
+        actions.andExpect(status().isNoContent())
+                .andDo(
+                        document(
+                                "delete-member",
+                                getRequestPreProcessor(),
+                                getResponsePreProcessor(),
+                                pathParameters(
+                                        getMemberRequestPathParameterDescriptor()
+                                )
+                        )
+                );
 
-                //given
+        //given
 //            MemberDto.Post post = new MemberDto.Post("vdcf2000@gmail.com",
 //                    "asdf123","강신찬");
 //            Member member = mapper.memberPostDtoToMember(post);
