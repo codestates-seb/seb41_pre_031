f72c5dea7fa6cdd54ee2b413b22966b508d0bfdb
xxyeon129 <xxyeon129@gmail.com>
2022-12-23 11:32:21 +0900

    [FE feat] 페이지네이션 기능 수정

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 const PageContainer = styled.span``;
 
+const Dot = styled.span`
+    padding: 0 0.8rem;
+`;
+
 const Button = styled.button`
     margin: 0 0.2rem;
 
@@ -23,6 +27,11 @@ const Pagination = ({ total, limit, page, setPage }) => {
     // 필요한 페이지의 개수 -> 개수만큼 루프 돌면서 페이지 번호 버튼 출력
     const pagesNum = Math.ceil(total / limit);
 
+    const handleBtnClick = (event) => {
+        setPage(Number(event.target.innerText));
+        window.scrollTo({ top: 0 });
+    };
+
     return (
         <>
             <PageContainer>
@@ -36,7 +45,116 @@ const Pagination = ({ total, limit, page, setPage }) => {
                         Prev
                     </Button>
                 )}
-                {Array(pagesNum)
+                {pagesNum > 5 && page < pagesNum - 3 && page > 4 ? (
+                    <>
+                        <Button
+                            key={1}
+                            onClick={() => {
+                                setPage(1);
+                                window.scrollTo({ top: 0 });
+                            }}
+                            aria-current={page === pagesNum ? "page" : null}
+                        >
+                            1
+                        </Button>
+                        <Dot>...</Dot>
+                        {Array(5)
+                            .fill()
+                            .map((el, index) => (
+                                <Button
+                                    key={page - 2 + index}
+                                    onClick={handleBtnClick}
+                                    aria-current={
+                                        page - 2 + index === page
+                                            ? "page"
+                                            : null
+                                    }
+                                >
+                                    {page - 2 + index}
+                                </Button>
+                            ))}
+                        <Dot>...</Dot>
+                        <Button
+                            key={pagesNum}
+                            onClick={() => setPage(pagesNum)}
+                            aria-current={page === pagesNum ? "page" : null}
+                        >
+                            {pagesNum}
+                        </Button>
+                    </>
+                ) : pagesNum > 5 && page >= pagesNum - 3 && page > 5 ? (
+                    <>
+                        <Button
+                            key={1}
+                            onClick={() => {
+                                setPage(1);
+                                window.scrollTo({ top: 0 });
+                            }}
+                            aria-current={page === 1 ? "page" : null}
+                        >
+                            1
+                        </Button>
+                        <Dot>...</Dot>
+                        {Array(5)
+                            .fill()
+                            .map((el, index) => (
+                                <Button
+                                    key={pagesNum - 4 + index}
+                                    onClick={handleBtnClick}
+                                    aria-current={
+                                        pagesNum - 4 + index === page
+                                            ? "page"
+                                            : null
+                                    }
+                                >
+                                    {pagesNum - 4 + index}
+                                </Button>
+                            ))}
+                    </>
+                ) : pagesNum <= 5 ? (
+                    Array(pagesNum)
+                        .fill()
+                        .map((el, index) => (
+                            <Button
+                                key={index + 1}
+                                onClick={handleBtnClick}
+                                aria-current={
+                                    page === index + 1 ? "page" : null
+                                }
+                            >
+                                {index + 1}
+                            </Button>
+                        ))
+                ) : (
+                    <>
+                        {Array(5)
+                            .fill()
+                            .map((el, index) => (
+                                <Button
+                                    key={index + 1}
+                                    onClick={handleBtnClick}
+                                    aria-current={
+                                        page === index + 1 ? "page" : null
+                                    }
+                                >
+                                    {index + 1}
+                                </Button>
+                            ))}
+                        <Dot>...</Dot>
+                        <Button
+                            key={pagesNum}
+                            onClick={() => {
+                                setPage(pagesNum);
+                                window.scrollTo({ top: 0 });
+                            }}
+                            aria-current={page === pagesNum ? "page" : null}
+                        >
+                            {pagesNum}
+                        </Button>
+                    </>
+                )}
+
+                {/* {Array(pagesNum)
                     .fill()
                     .map((el, index) => (
                         <Button
@@ -49,16 +167,18 @@ const Pagination = ({ total, limit, page, setPage }) => {
                         >
                             {index + 1}
                         </Button>
-                    ))}
-                <Button
-                    onClick={() => {
-                        setPage(page + 1);
-                        window.scrollTo({ top: 0 });
-                    }}
-                    disabled={page === pagesNum}
-                >
-                    Next
-                </Button>
+                    ))} */}
+                {page === pagesNum ? null : (
+                    <Button
+                        onClick={() => {
+                            setPage(page + 1);
+                            window.scrollTo({ top: 0 });
+                        }}
+                        disabled={page === pagesNum}
+                    >
+                        Next
+                    </Button>
+                )}
             </PageContainer>
         </>
     );
