7ec7dd9a52f7676febaacb076de7d867f1533da2
kimjeonghui <20190896@sungshin.ac.kr>
2022-12-25 14:32:11 +0900

    [BE feat] question Entity

--- server/pre_project/src/main/java/com/codestates/backend/pre_project/exception/ExceptionCode.java
+++ server/pre_project/src/main/java/com/codestates/backend/pre_project/exception/ExceptionCode.java
@@ -4,13 +4,15 @@ import lombok.Getter;
 
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
-    MEMBER_EXISTS(409, "Member exists");
+    MEMBER_EXISTS(409, "Member exists"),
     //COFFEE_NOT_FOUND(404, "Coffee not found"),
     //COFFEE_CODE_EXISTS(409, "Coffee Code exists"),
     //ORDER_NOT_FOUND(404, "Order not found"),
     //CANNOT_CHANGE_ORDER(403, "Order can not change"),
     //NOT_IMPLEMENTATION(501, "Not Implementation"),
     //INVALID_MEMBER_STATUS(400, "Invalid member status");  // TO 추가된 부분
+
+    QUESTION_NOT_FOUND(404, "Qustion not found");
 
     @Getter
     private int status;
