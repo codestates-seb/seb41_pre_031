1b343cf773e02876a756df336548c667d4e15a48
kimjeonghui <20190896@sungshin.ac.kr>
2022-12-25 14:37:29 +0900

    [BE feat] question Dto

--- server/pre_project/src/main/java/com/codestates/backend/pre_project/post/question/dto/QuestionDto.java
+++ server/pre_project/src/main/java/com/codestates/backend/pre_project/post/question/dto/QuestionDto.java
@@ -1,4 +1,61 @@
 package com.codestates.backend.pre_project.post.question.dto;
 
+import com.codestates.backend.pre_project.post.question.QuestionTag;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Set;
+
 public class QuestionDto {
+
+    @Getter
+    public static class Post{
+            private long questionId;
+
+            private String qustionTitle;
+
+            private String questionBody;
+
+            private long memberId;
+
+            @Valid
+            private List<QuestionTagDto> questionTags;
+    }
+
+    public static class Patch{
+        private long questionId;
+
+        private long memberId;
+
+        private String questionTitle;
+
+        private String questionBody;
+
+        @Valid
+        private List<QuestionTagDto> questionTags;
+
+    }
+
+    public static class Response{
+        private long questionId;
+
+        private long memberId;
+
+        private String questionTitle;
+
+        private String questionBody;
+
+        @Valid
+        private List<QuestionTagDto> questionTags;
+
+        private LocalDateTime questionRegDate;
+
+        private LocalDateTime questionLastDate;
+
+    }
 }
