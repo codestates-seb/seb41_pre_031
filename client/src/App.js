90f6c5e4ecc22a1e624b6907f927a30e971f87db
rlawodn46465 <rlawodn46465@naver.com>
2022-12-27 00:22:38 +0900

    [FE misc] full

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { BREAK_POINT_MOBILE } from "./data/breakpoints";
 import UserProfile from "./pages/UserProfile";
 import { Routes, Route } from "react-router";
+import AllQuestions from "./pages/AllQuestions";
 
 const Wrap = styled.div`
     display: flex;
@@ -71,6 +72,7 @@ function App() {
                             path="/users"
                             element={<Users />}
                         />
+                        <Route path="/questions" element={<AllQuestions/>}/>
                     </Routes>
                 </Section>
             </Main>
