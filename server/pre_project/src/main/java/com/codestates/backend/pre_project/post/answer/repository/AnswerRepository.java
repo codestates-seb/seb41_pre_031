0c06814fb20925583479efeea900ddc917e55a10
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-12-28 00:18:47 +0900

    Delete server/pre_project directory

--- server/pre_project/src/main/java/com/codestates/backend/pre_project/post/answer/repository/AnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codestates.backend.pre_project.post.answer.repository;
-
-
-import com.codestates.backend.pre_project.post.answer.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
--- server/pre_project/src/main/java/com/codestates/backend/pre_project/post/comment/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codestates.backend.pre_project.post.comment.repository;
-
-import com.codestates.backend.pre_project.post.comment.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
