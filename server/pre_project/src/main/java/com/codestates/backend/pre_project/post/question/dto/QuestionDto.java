8d4b19978277ade0e7ac3d8857b6d7620effd966
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-12-27 09:10:16 +0900

    Be/feat/question (#49)
    * [BE feat] question Entity
    
    * [BE feat] question Repository
    
    * [BE feat] question Service
    
    * [BE feat] question Dto
    
    * [BE feat] question Mapper
    
    * [BE fix] question Mapper 수정
    
    * [BE fix] question Mapper, Service, QuestionTagResponseDto, QuestionDto 수정
    
    * [BE feat] QuestionController 구현
    
    * [BE feat] question test 구현
    
    Co-authored-by: kimjeonghui <20190896@sungshin.ac.kr>

--- server/pre_project/src/main/java/com/codestates/backend/pre_project/post/question/dto/QuestionDto.java
+++ server/pre_project/src/main/java/com/codestates/backend/pre_project/post/question/dto/QuestionDto.java
@@ -1,4 +1,73 @@
 package com.codestates.backend.pre_project.post.question.dto;
 
+import com.codestates.backend.pre_project.post.question.QuestionTag;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class QuestionDto {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Post{
+
+            private String qustionTitle;
+
+            private String questionBody;
+
+            private long memberId;
+
+            @Valid
+            private List<QuestionTagDto> questionTags;
+
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Patch{
+        private long questionId;
+
+        private long memberId;
+
+        private String questionTitle;
+
+        private String questionBody;
+
+        @Valid
+        private List<QuestionTagDto> questionTags;
+
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Setter
+    public static class Response{
+        private long questionId;
+
+        private long memberId;
+
+        private String questionTitle;
+
+        private String questionBody;
+
+        @Valid
+        private List<QuestionTagResponseDto> questionTags;
+
+        private LocalDateTime questionRegDate;
+
+        private LocalDateTime questionLastDate;
+
+        private long questionLikes;
+
+    }
 }
