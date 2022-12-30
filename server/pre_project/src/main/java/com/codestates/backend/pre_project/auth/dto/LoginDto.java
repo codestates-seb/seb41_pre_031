a227925343388b956530d327fd8ba75cfe71e16f
jeonghui <59547003+kimjeonghui@users.noreply.github.com>
2022-12-30 22:39:38 +0900

    be/fix/vote (#79)
    Co-authored-by: 김재우(Kim jaewoo) <48310143+rlawodn46465@users.noreply.github.com>

--- server/pre_project/src/main/java/com/codestates/backend/pre_project/auth/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.codestates.backend.pre_project.auth.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginDto {
-    private String username;
-    private String password;
-}
--- server/pre_project/src/main/java/com/codestates/backend/pre_project/profile/repository/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.backend.pre_project.profile.repository;
-
-import com.codestates.backend.pre_project.profile.entity.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
